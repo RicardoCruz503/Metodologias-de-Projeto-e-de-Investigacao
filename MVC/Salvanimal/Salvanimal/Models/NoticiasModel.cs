5989e1d2718603bd4e00e199ee46e5d723528761
Ricardo Cruz <ricardoc.94911@gmail.com>
2015-11-25 21:04:04 +0000

    no message

---
+++ MVC/Salvanimal/Salvanimal/Models/NoticiasModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace Salvanimal.Models
+{
+    public class NoticiasContext : DbContext
+    {
+        public NoticiasContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<NoticiasEntity> NoticiasProfiles { get; set; }
+    }
+
+    [Table("NoticiasEntity")]
+    public class NoticiasEntity
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int NoticiaId { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "A {0} tem de ter pelo menos {2} caracteres.", MinimumLength = 6)]
+        public string Titulo { get; set; }
+        [Required]
+        [StringLength(5000, ErrorMessage = "A {0} tem de ter pelo menos {2} caracteres.", MinimumLength = 50)]
+        public string CorpoNoticia { get; set; }
+        [Required]
+        public DateTime data { get; set; }
+    }
+}
